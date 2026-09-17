a9e56d615f1e46371754dabb8a5ac9a0037504db
kuka01 <kuanyshtashbolat@gmail.com>
2026-09-17 11:39:41 +0500

    Remove unused Dart starter files

--- Practice_2/bin/practice_2.dart
+++
@@ -1,5 +0,0 @@
-import 'package:practice_2/practice_2.dart' as practice_2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${practice_2.calculate()}!');
-}
--- Practice_2/test/practice_2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:practice_2/practice_2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
